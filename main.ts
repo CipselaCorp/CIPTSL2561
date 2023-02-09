91d350e2ce002b8085a91e3f3a8595d6d66020e6
CipselaCorp <124727402+CipselaCorp@users.noreply.github.com>
2023-02-09 15:32:22 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -34,14 +34,14 @@ namespace CIPLUX {
     //% blockId="CIPLUX"
     //% block="Leer LUX"
 export function LUX(): number {
-    set_Reg_num(GAIN_ACCES, 0x11);
+    set_Reg_num(GAIN_ACCES, INTEGRATION_TIME);
     basic.pause(10);
     let ch00 = get2Reg(CH0_ACCES_LOW);
     basic.pause(1000);
     let ch01 = get2Reg(CH0_ACCES_UP);
     //let data = pins.i2cReadNumber(TSL2561_I2C_ADRESS, NumberFormat.UInt16BE, false)
     //lux = 256*(ch0+ ch1)
-    return ch00 + ch01
+    return ch00
 }
 
 }
