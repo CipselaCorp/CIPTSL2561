f48fc47871e1c970c1151a981748ac331a1a7a5c
CipselaCorp <124727402+CipselaCorp@users.noreply.github.com>
2023-02-09 15:22:17 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,15 @@
 let lux = 0
-let TSL2561_I2C_ADRESS = 0x39
-let GAIN_ACCES = 0x81
-let INTEGRATION_TIME = 0X11
-let CH0_ACCES_LOW = 0X8C
-let CH0_ACCES_UP = 0X8D
+const TSL2561_I2C_ADRESS = 0x39
+const GAIN_ACCES = 0x81
+const INTEGRATION_TIME = 0X11
+const CH0_ACCES_LOW = 0X8C
+const CH0_ACCES_UP = 0X8D
+let ch0= 0;
+let ch1 = 0;
 
 function set_Reg_num(reg: number, dat: number): void {
     let _wbuf = pins.createBuffer(2);
-    _wbuf[0] = reg | 0x0A;
+    _wbuf[0] = reg;
     _wbuf[1] = dat;
     pins.i2cWriteBuffer(TSL2561_I2C_ADRESS, _wbuf);
 }
@@ -21,38 +23,26 @@ function get2Reg(reg: number): number {
     return pins.i2cReadNumber(TSL2561_I2C_ADRESS, NumberFormat.UInt16BE);
 }
 
-function set_Reg(command: number): number {
-    let buf = pins.createBuffer(2);
-    buf[0] = command >> 16
-    buf[1] = command & 0xffff
-    return pins.i2cWriteBuffer(TSL2561_I2C_ADRESS, buf)
-}
 
 //% color=#9C36B5 weight=25 icon="\uf005" block="CIPLUX2560"
 namespace CIPLUX {
-    
+    set_Reg_num(GAIN_ACCES, 0x11);
+    basic.pause(10)
     /**
             * Returns a number describing the LUX intensity
         */
     //% blockId="CIPLUX"
     //% block="Leer LUX"
 export function LUX(): number {
-    //set_Reg_num(GAIN_ACCES, 0x11);
-    basic.pause(10)
-    set_Reg(CH0_ACCES_LOW)
-    let ch0 = pins.i2cReadBuffer(TSL2561_I2C_ADRESS,pins.sizeOf(NumberFormat.UInt16LE) * 15)
-    let result = ch0[0] << 16;
-    result |= ch0[1];
+   
+    
+    let ch00 = get2Reg(CH0_ACCES_LOW)
     basic.pause(1000)
-    set_Reg(CH0_ACCES_UP)
-    basic.pause(10)
-    let ch1 = pins.i2cReadBuffer(TSL2561_I2C_ADRESS, pins.sizeOf(NumberFormat.UInt16LE) * 15)
-    let result_1 = ch1[0] << 16;
-    result_1 |= ch1[1];
-    basic.pause(100)
+    
+    let ch01 = get2Reg(CH0_ACCES_UP)
     //let data = pins.i2cReadNumber(TSL2561_I2C_ADRESS, NumberFormat.UInt16BE, false)
     //lux = 256*(ch0+ ch1)
-    return result + result_1
+    return ch00 + ch01
 }
 
 }
